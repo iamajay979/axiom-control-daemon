b8f691a83baaa635bf87317e9147f7e604f54631
Kiquance <kiquance21@gmail.com>
2019-06-11 11:32:41 +0530

    Bug fixes

--- Adapters/smbus.cpp
+++ Adapters/smbus.cpp
@@ -13,11 +13,12 @@
 
 #include <errno.h>
 #include <stddef.h>
-#include <smbus.h>
 #include <sys/ioctl.h>
 #include <linux/types.h>
 #include <linux/i2c.h>
 #include <linux/i2c-dev.h>
+
+#include "smbus.h"
 
 /* Compatibility defines */
 #ifndef I2C_SMBUS_I2C_BLOCK_BROKEN
